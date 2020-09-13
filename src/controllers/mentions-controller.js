aafb0c92575a4721784caef2a640fb8e746a7517
Glauco Fox <glauco.fox@gmail.com>
2020-09-12 21:31:01 -0300

    Added update functionality

--- src/controllers/mentions-controller.js
+++ src/controllers/mentions-controller.js
@@ -28,6 +28,30 @@ exports.createMention = async(req, res) => {
         res.status(201).send({message: 'Menção cadastrada com sucesso'});
 
     } catch (e) {
-        res.status(500).send({message: 'Falha ao cadastrar a menção: ' + e});
+        res.status(500).send({message: 'Falha ao cadastrar a menção.'});
     }
+};
+
+// update
+exports.updateMention = async(req, res) => {
+
+    const {errors} = validationResult(req);
+
+    if (errors.length > 0) {
+        return res.status(400).send({message: errors});
+    }
+
+    try {
+        await repository.updateMention(req.params.id, req.body);
+        res.status(200).send({
+            message: 'Menção atualizada com sucesso!'
+        });
+    } catch (e) {
+        res.status(500).send({message: 'Falha ao atualizar a menção.'})
+    }
+};
+
+// delete
+exports.deleteMention = async(req, res) => {
+    
 };
